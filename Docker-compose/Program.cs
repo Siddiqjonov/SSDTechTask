77e0538c13803b43f8e71022ad9b140610874c22
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-07-14 07:43:58 +0500

    some fixes were made

---
+++ Docker-compose/Program.cs
@@ -0,0 +1,14 @@
+namespace Docker_compose
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddHostedService<Worker>();
+
+            var host = builder.Build();
+            host.Run();
+        }
+    }
+}
